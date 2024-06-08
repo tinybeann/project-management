cc4cf0ddfcb920ad5acd6b0d8bb024e0b98f1231
Lê Bá Dũng <lebadung178@gmail.com>
2024-06-08 07:37:20 +0700

    Create project

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("client/pages/home/index");
+})
+
+app.get("/products", (req, res) => {
+  res.render("client/pages/products/index");
+})
+
+app.listen(port, () => {
+  console.log(`app listening in port ${port}`);
+})
