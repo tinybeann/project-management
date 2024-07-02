8212131a42cb843c66ee9c49b7d7c5f79dc37abc
Lê Bá Dũng <lebadung178@gmail.com>
2024-07-02 22:41:24 +0700

    Cấu hình .env

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const routesClient = require("./routes/client/index.route");
 
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
