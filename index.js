6974da97b76383855b33449b96e3c2724f2942f4
Lê Bá Dũng <lebadung178@gmail.com>
2024-07-23 15:03:30 +0700

    Cấu hình Database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const routesClient = require("./routes/client/index.route");
+const database = require("./config/database");
 
 dotenv.config();
+
+database.connect();
+
+const routesClient = require("./routes/client/index.route");
 
 const app = express();
 const port = process.env.PORT;
