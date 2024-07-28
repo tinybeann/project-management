f5f68a717187298ff262c6425c3f528ac0b78278
Lê Bá Dũng <lebadung178@gmail.com>
2024-07-28 15:54:27 +0700

    Lấy data sản phẩm in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,12 @@
+const Product = require("../../models/product.model");
 // [GET] /admin/products/
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false
+  })
+
   res.render("admin/pages/products/index", {
-    pageTitle: "Danh sách sản phẩm"
+    pageTitle: "Danh sách sản phẩm",
+    products: products
   });
 }
