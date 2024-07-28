b3b0bd44354a78fd3d8c7e48e1adda790f4536a4
Lê Bá Dũng <lebadung178@gmail.com>
2024-07-28 17:00:24 +0700

    Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,12 +1,45 @@
 const Product = require("../../models/product.model");
 // [GET] /admin/products/
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
+  const find = {
     deleted: false
-  })
+  }
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const products = await Product.find(find);
+
+  const filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: ""
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: ""
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: ""
+    }
+  ];
+
+  if(req.query.status) {
+    const index = filterStatus.findIndex(item => item.status == req.query.status);
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex(item => item.status == "");
+    filterStatus[index].class = "active";  
+  }
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
-    products: products
+    products: products,
+    filterStatus: filterStatus
   });
 }
