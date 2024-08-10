1767c2a04fca0a78c55fd4c8ad127b38b4d848b9
Lê Bá Dũng <lebadung178@gmail.com>
2024-08-10 09:44:15 +0700

    Thêm try catch cho code

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,42 +1,48 @@
 const Product = require("../../models/product.model");
 const filterHelper = require("../../helpers/filter.helper");
 const paginationHelper = require("../../helpers/pagination.helper");
+const systemConfig = require("../../config/system");
 // [GET] /admin/products/
 module.exports.index = async (req, res) => {
-  const find = {
-    deleted: false
-  }
-
-  if (req.query.status) {
-    find.status = req.query.status;
-  }
-
-  //  Filter 
-  const filterStatus = filterHelper(req);
-  // End Filter
+  try {
+    const find = {
+      deleted: false
+    }
   
-  // Search
-  if (req.query.keyword) {
-    const regex = new RegExp(req.query.keyword, "i");
-    find.title = regex;
+    if (req.query.status) {
+      find.status = req.query.status;
+    }
+  
+    //  Filter 
+    const filterStatus = filterHelper(req);
+    // End Filter
+    
+    // Search
+    if (req.query.keyword) {
+      const regex = new RegExp(req.query.keyword, "i");
+      find.title = regex;
+    }
+    // End Search 
+  
+    // Pagination
+    const countRecords = await Product.countDocuments(find);
+    const objectPagination = paginationHelper(req, countRecords);
+    // End Pagination
+  
+    const products = await Product
+      .find(find)
+      .limit(objectPagination.limitItems)
+      .skip(objectPagination.skip);  
+  
+    res.render("admin/pages/products/index", {
+      pageTitle: "Danh sách sản phẩm",
+      products: products,
+      filterStatus: filterStatus,
+      keyword: req.query.keyword,
+      objectPagination: objectPagination
+    });
+  } catch(error) {
+    console.log(error);
+    res.redirect(`/${systemConfig.prefixAdmin}/dashboard`); 
   }
-  // End Search 
-
-  // Pagination
-  const countRecords = await Product.countDocuments(find);
-  const objectPagination = paginationHelper(req, countRecords);
-  // End Pagination
-
-  const products = await Product
-    .find(find)
-    .limit(objectPagination.limitItems)
-    .skip(objectPagination.skip);  
-
-  res.render("admin/pages/products/index", {
-    pageTitle: "Danh sách sản phẩm",
-    products: products,
-    filterStatus: filterStatus,
-    keyword: req.query.keyword,
-    objectPagination: objectPagination
-  });
 }
