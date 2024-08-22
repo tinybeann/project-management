0a08c06b31e7015a1d4f384555870560f8a66b39
Lê Bá Dũng <lebadung178@gmail.com>
2024-08-22 16:50:57 +0700

    Xử lý ẩn thông báo bên Frontend

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -178,3 +178,22 @@ if (listButtonDelete.length > 0) {
   });
 }
 // End button-delete
+
+// show-alert
+const showAlert = document.querySelector("[show-alert]");
+if (showAlert) {
+  let time = showAlert.getAttribute("data-time");
+  time = parseInt(time);
+
+  // Sau time giây sẽ đóng thông báo
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden");
+  }, time);
+
+  // Khi click vào nút close-alert sẽ đóng luôn
+  const closeAlert = showAlert.querySelector("[close-alert]");
+  closeAlert.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden");
+  });
+}
+// End show-alert
