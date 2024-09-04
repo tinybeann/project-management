7e276ec2df6a47a47220c1e437eae24aab42d44f
Lê Bá Dũng <lebadung178@gmail.com>
2024-09-04 15:30:25 +0700

    Tính năng tạo mới sản phẩm (Thêm slug)

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 
 const productSchema = new mongoose.Schema({
   title: String,
@@ -9,6 +11,11 @@ const productSchema = new mongoose.Schema({
   thumbnail: String,
   status: String,
   position: Number,
+  slug: {
+    type: String,
+    slug: "title",
+    unique: true
+  },
   deleted: {
     type: Boolean,
     default: false
