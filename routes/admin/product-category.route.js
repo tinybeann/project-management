b8f634be3108c1f36cafc4f5963e7ec8bb9bb42d
Lê Bá Dũng <lebadung178@gmail.com>
2024-10-30 13:07:59 +0700

    Xây dựng phần quản lý danh mục sản phẩm (Thêm mới danh mục sản phẩm)

---
+++ routes/admin/product-category.route.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const multer = require('multer');
+const router = express.Router();
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
+const upload = multer();
+
+const controller = require("../../controllers/admin/product-category.controller");
+
+router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+  "/create",
+  upload.single('thumbnail'),
+  uploadCloud.uploadSingle,
+  controller.createPost
+);
+
+module.exports = router;
