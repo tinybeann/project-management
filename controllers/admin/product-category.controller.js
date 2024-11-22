88b3f27cedc36ce812d6a4774eb93643d93e7f1a
Lê Bá Dũng <lebadung178@gmail.com>
2024-11-22 17:30:58 +0700

    Tính năng chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,7 +1,7 @@
 const ProductCategory = require("../../models/product-category.model");
 
 const systemConfig = require("../../config/system");
-const creatTreeHelper = require("../../helpers/createTree.helper");
+const createTreeHelper = require("../../helpers/createTree.helper");
 
 // [GET] /admin/products-category/
 module.exports.index = async (req, res) => {
@@ -41,4 +41,40 @@ module.exports.createPost = async (req, res) => {
   await record.save();
 
   res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
+};
+
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const find = {
+      _id: req.params.id,
+      deleted: false,
+    };
+    const data = await ProductCategory.findOne(find);
+    const records = await ProductCategory.find({
+      deleted: false,
+    });
+    const newRecords = createTreeHelper(records); 
+    res.render("admin/pages/products-category/edit", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
+  }
+};
+
+// [PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  req.body.position = parseInt(req.body.position);
+  try {
+    await ProductCategory.updateOne({ _id: id }, req.body);
+    req.flash("success", `Cập nhật danh mục thành công!`);
+  } catch (error) {
+    req.flash("error", `Cập nhật danh mục không thành công!`);
+  }
+  res.redirect("back");
 };
