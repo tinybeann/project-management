944d5d4ab7980d1c4e7e784a6bec7931519eabd7
Lê Bá Dũng <lebadung178@gmail.com>
2025-01-01 02:18:35 +0700

    Xây dựng phần chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -31,3 +31,33 @@ module.exports.createPost = async (req, res) => {
 
   res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit= async (req, res) => {
+  try {
+    const data = await Role.findOne({
+      _id: req.params.id,
+      deleted: false,
+    });
+
+    res.render("admin/pages/roles/edit", {
+      pageTitle: "Chỉnh sửa nhóm quyền",
+      data: data
+    });
+  } catch (error) {
+    res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+  }
+}
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Role.updateOne({ _id: id }, req.body);
+    req.flash("success", `Cập nhật nhóm quyền thành công!`);
+  } catch (error) {
+    req.flash("error", `Cập nhật nhóm quyền không thành công!`);
+  }
+
+  res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+}
