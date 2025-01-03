a09395f0191a11f5375286ab967ccf82b4c10c0a
Lê Bá Dũng <lebadung178@gmail.com>
2025-01-03 18:03:29 +0700

    Trang tạo tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,6 +1,12 @@
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model")
+const md5 = require("md5");
 
-// [GET] /admin/account/
+const generateHelper = require("../../helpers/generate.helper");
+
+const systemConfig = require("../../config/system");
+
+// [GET] /admin/accounts/
 module.exports.index = async (req, res) => {
   // Find
   let find = {
@@ -9,9 +15,31 @@ module.exports.index = async (req, res) => {
   // End Find
   
   const records = await Account.find(find);
-  console.log(records);
-  res.render("admin/pages/accounts", {
+  res.render("admin/pages/accounts/index", {
     pageTitle: "Danh sách tài khoản",
     records: records
   });
+}
+
+// [GET] /admin/accounts/create
+module.exports.create = async (req, res) => {
+  const roles = await Role.find({
+    deleted: false,
+  });
+
+  res.render("admin/pages/accounts/create", {
+    pageTitle: "Tạo mới tài khoản",
+    roles: roles
+  })
+}
+
+// [POST] /admin/accounts/create
+module.exports.createPost = async (req, res) => {
+  req.body.password = md5(req.body.password);
+  req.body.token = generateHelper.generateRandomString(30);
+
+  const account = new Account(req.body);
+  await account.save();
+
+  res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
 }
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const multer = require('multer');
 const router = express.Router();
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 const controller = require("../../controllers/admin/account.controller");
 
+const validate = require("../../validates/admin/account.validate");
+
+const upload = multer();
+
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+  "/create", 
+  upload.single('avatar'),
+  uploadCloud.uploadSingle,
+  validate.createPost,
+  controller.createPost
+);
 
 module.exports = router;
