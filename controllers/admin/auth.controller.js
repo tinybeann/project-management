f29bbf619f08ce40f74042d1eb85fdbe89da9c24
Lê Bá Dũng <lebadung178@gmail.com>
2025-01-24 16:07:46 +0700

    Tính năng đăng nhập(kiểm tra data gửi lên đúng hay sai)

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -1,11 +1,43 @@
+const md5 = require("md5");
+const Account = require("../../models/account.model");
+
+const systemConfig = require("../../config/system");
+
 // [GET] /admin/auth/login
 module.exports.login = async (req, res) => {
   res.render("admin/pages/auth/login", {
     pageTitle: "Đăng nhập",
   });
 };
+
 // [POST] /admin/auth/login
 module.exports.loginPost = async (req, res) => {
-  console.log(req.body);
-  res.send("OK");
+  const email = req.body.email;
+  const password = md5(req.body.password);
+
+  const user = await Account.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if(!user) {
+    req.flash("error", "Email không tồn tại");
+    res.redirect("back");
+    return;
+  }
+
+  if(user.password != password) {
+    req.flash("error", "Sai mật khẩu");
+    res.redirect("back");
+    return;
+  }
+
+  if (user.status != "active") {
+    req.flash("error", "Tài khoản đang bị khóa!");
+    res.redirect("back");
+    return;
+  }
+  
+  res.cookie("token", user.token);
+  res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);
 };
