2603ab9f20e1fd3341fbc3d98860e1d2514f6815
Lê Bá Dũng <lebadung178@gmail.com>
2025-01-24 15:36:50 +0700

    Tính năng đăng nhập(gửi data lên backend)

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const productRoutes = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
+const authRoutes = require("./auth.route");
 const systemConfig = require("../../config/system");
 
 module.exports = (app) => {
@@ -17,4 +18,6 @@ module.exports = (app) => {
   app.use(`/${PATH_ADMIN}/roles`, roleRoutes);
 
   app.use(`/${PATH_ADMIN}/accounts`, accountRoutes);
+
+  app.use(`/${PATH_ADMIN}/auth`, authRoutes);
 }
